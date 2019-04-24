a02b89f53b2dcb74260ec46da0d36e2595d03146
nishidakousuke <nishidahikariyuu@nishidahikariyuunoMacBook-Air.local>
2019-04-24 18:43:51 +0900

    Text change

--- chat-space/app/assets/javascripts/delete.js
+++ chat-space/app/assets/javascripts/delete.js
@@ -1,14 +1,7 @@
 $(function(){
     $(document).on("click", ".user-search-remove", function(){
         var chat_user_id = ($(this).attr('id'));
-        $.ajax({
-            type: 'GET',
-            url: '/users',
-            dataType: 'json'
-        })
-        .done(function(users){
-            var target = document.getElementById(Number(chat_user_id));
-            $(target).parent().remove();
+        var target = document.getElementById(Number(chat_user_id));
+        $(target).parent().remove();
         })
     })
-})
