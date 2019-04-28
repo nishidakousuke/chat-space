49d475631699c5c7868cf1a4757dd3376fe45084
nishidakousuke <nishidahikariyuu@nishidahikariyuunoMacBook-Air.local>
2019-04-28 16:42:09 +0900

    Text change

--- chat-space/config/deploy.rb
+++ chat-space/config/deploy.rb
@@ -1,15 +1,15 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock '~> 3.11.0'
 
-set :application, "chat-space"
-set :repo_url, "git@github.com:nishidakousuke/chat-space.git"
+set :application, 'chat-space'
+set :repo_url, 'git@github.com:nishidakousuke/chat-space.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/.ssh/nishidak.pem']
+                  keys: ['~/.ssh/nishidak.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
