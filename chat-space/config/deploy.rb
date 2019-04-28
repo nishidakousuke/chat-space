f417f79358ae37ecf47ce6ad6579a13c59e40912
nishidakousuke <nishidahikariyuu@nishidahikariyuunoMacBook-Air.local>
2019-04-29 00:03:27 +0900

    add

--- chat-space/config/deploy.rb
+++ chat-space/config/deploy.rb
@@ -12,7 +12,7 @@ set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/nishidak.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :unicorn_config_path, -> { "#{current_path}/#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 after 'deploy:publishing', 'deploy:restart'
